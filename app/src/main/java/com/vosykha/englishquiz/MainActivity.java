a80d1856b4b245069f7beaa4d5dcc79c6ca79d97
khaheo2528 <72848442+khaheo2528@users.noreply.github.com>
2021-05-29 19:21:15 +0700

    writing

--- app/src/main/java/com/vosykha/englishquiz/MainActivity.java
+++ app/src/main/java/com/vosykha/englishquiz/MainActivity.java
@@ -1,10 +1,12 @@
 package com.vosykha.englishquiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
