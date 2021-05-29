b717616de72eb58396c923b949a180dec32b3e0d
khaheo2528 <72848442+khaheo2528@users.noreply.github.com>
2021-05-29 23:24:32 +0700

    updateLib2

--- app/src/main/java/com/vosykha/englishquiz/MainActivity.java
+++ app/src/main/java/com/vosykha/englishquiz/MainActivity.java
@@ -1,15 +1,15 @@
 package com.vosykha.englishquiz;
 
 import android.os.Bundle;
-import android.support.v7.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_main);
+        setContentView(R.layout.activity_main);
     }
 }
