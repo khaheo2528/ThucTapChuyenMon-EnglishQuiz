3dd2bf0ee745ea2e9b6eb7cbd976778324f7c3ed
khaheo2528 <72848442+khaheo2528@users.noreply.github.com>
2021-05-30 20:25:18 +0700

    create_design4form

---
+++ app/src/main/java/com/vosykha/englishquiz/Time_Up.java
@@ -0,0 +1,14 @@
+package com.vosykha.englishquiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Time_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_up);
+    }
+}
