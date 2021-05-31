14cd6e6ec1dca49ec5e8f00e1c328003eca7eeed
khaheo2528 <72848442+khaheo2528@users.noreply.github.com>
2021-05-31 09:49:26 +0700

    createClass

--- app/src/androidTest/java/com/vosykha/englishquiz/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vosykha/englishquiz/ExampleInstrumentedTest.java
@@ -1,14 +1,13 @@
 package com.vosykha.englishquiz;
 
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +15,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
         assertEquals("com.vosykha.englishquiz", appContext.getPackageName());
     }
 }
